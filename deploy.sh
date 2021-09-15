af2373161c61a1c36533837d21b73c4d3e92b5f7
indranilatcal <indranil.chatterjee@dudesolutions.com>
2021-09-15 22:38:03 +0530

    overwrote the entire folder

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,16 @@
-docker build -t indranilatcal/multi-client:latest -t indranilatcal/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t indranilatcal/multi-server:latest -t indranilatcal/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t indranilatcal/multi-worker:latest -t indranilatcal/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push indranilatcal/multi-client:latest
-docker push indranilatcal/multi-server:latest
-docker push indranilatcal/multi-worker:latest
+docker build -t cygnetops/multi-client-k8s:latest -t cygnetops/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
+docker build -t cygnetops/multi-server-k8s-pgfix:latest -t cygnetops/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
+docker build -t cygnetops/multi-worker-k8s:latest -t cygnetops/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
 
-docker push indranilatcal/multi-client:$SHA
-docker push indranilatcal/multi-server:$SHA
-docker push indranilatcal/multi-worker:$SHA
+docker push cygnetops/multi-client-k8s:latest
+docker push cygnetops/multi-server-k8s-pgfix:latest
+docker push cygnetops/multi-worker-k8s:latest
+
+docker push cygnetops/multi-client-k8s:$SHA
+docker push cygnetops/multi-server-k8s-pgfix:$SHA
+docker push cygnetops/multi-worker-k8s:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/client-deployment client=indranilatcal/multi-client:$SHA
-kubectl set image deployments/server-deployment server=indranilatcal/multi-server:$SHA
-kubectl set image deployments/worker-deployment worker=indranilatcal/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=cygnetops/multi-server-k8s-pgfix:$SHA
+kubectl set image deployments/client-deployment client=cygnetops/multi-client-k8s:$SHA
+kubectl set image deployments/worker-deployment worker=cygnetops/multi-worker-k8s:$SHA
